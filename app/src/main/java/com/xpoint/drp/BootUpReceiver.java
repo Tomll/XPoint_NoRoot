7a6deb03b2332e319a3f183a27560c7c22923e1f
Tomll <drp5869@sina.com>
2018-07-26 17:44:01 +0800

    XPoint简版（无需Root）

---
+++ app/src/main/java/com/xpoint/drp/BootUpReceiver.java
@@ -0,0 +1,21 @@
+package com.xpoint.drp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by dongrp on 2017/2/20.
+ * 开机广播接收器
+ */
+
+public class BootUpReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)){
+            //接收到开机广播后，开启PointService
+            context.startService( new Intent(context,PointService.class));
+        }
+    }
+}
